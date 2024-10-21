009c1129dcba6666dc40c6c65f3670105da5cd2f
David E <128513280+axelE123@users.noreply.github.com>
2024-10-20 17:48:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,22 +12,30 @@ function initMap() {
           lng: -123.1444, 
           name: 'Sharon Fortney',
           image: 'https://images.squarespace-cdn.com/content/v1/58d29e6ccd0f6829bdf2f58f/0fb21d0e-d159-46d2-809d-d91fa9d5f0c0/Sharon+-0073.jpg?format=500w',
-          audio: 'audio/museum_of_vancouver.mp3'
+          audio: 'audio/sharon_fortney.mp3'
       },
       {
           lat: 49.2805, 
           lng: -123.1022, 
           name: 'Tsitsayxemaat - Rebecca Duncan',
           image: 'https://images.squarespace-cdn.com/content/v1/58d29e6ccd0f6829bdf2f58f/e4d2d469-efb5-4037-8933-d7fd3c554cdd/Rebecca-Duncan-1-1024x683.jpg?format=500w',
-          audio: 'audio/chinatown.mp3'
+          audio: 'audio/rebecca_duncan.mp3'
       },
       {
-          lat: 49.2713, 
+          lat: 49.2565, 
           lng: -123.1394, 
           name: 'Bill Darnell',
           image: 'https://historyofsocialchange.ca/wp-content/uploads/2015/07/bill-darnell.jpeg',
-          audio: 'audio/greenpeace.mp3'
-      }
+          audio: 'audio/bill_darnell.mp3'
+      },
+      {
+        lat: 49.280390, 
+        lng: -123.086920, 
+        name: 'Marie Kishchuk',
+        image: 'https://mail.google.com/mail/u/0?ui=2&ik=a33659397c&attid=0.2&permmsgid=msg-f:1797919248712795360&th=18f37e07ea5d04e0&view=att&disp=safe&realattid=18f37dfc9087bea04d62',
+        audio: 'audio/marie_kishchuk.mp3'
+    }
+      
   ];
 
   // Create markers and InfoWindows for each location
